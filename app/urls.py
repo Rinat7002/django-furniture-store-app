677dd3bbcc3eda5e72cc5d647ae9bbd2d11c4e09
Rinat7002 <rinat_idrisov_2016@mail.ru>
2024-03-06 19:49:12 +0500

    debug-toolbar added

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 
 
+from re import DEBUG
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,3 +9,8 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
+
+if DEBUG:
+    urlpatterns += [ 
+        path("__debug__/", include("debug_toolbar.urls")), 
+    ]
